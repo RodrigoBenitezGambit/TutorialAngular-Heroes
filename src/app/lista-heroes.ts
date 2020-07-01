b79712bdd59ce33fcb1c16b83fa35dd5e44e0190
rodrigobenitezseb <rodrigo.seb.benitez@gmail.com>
2020-07-01 16:52:12 -0300

    Avances con el tutorial de Heroes

---
+++ src/app/lista-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Dr Nice', skill:'Clonacion' },
+    { id: 12, name: 'Narco', skill:'Especialista en armas' },
+    { id: 13, name: 'Bombasto', skill:'Disparar bombas' },
+    { id: 14, name: 'Celeritas', skill:'Controlar las plantas' },
+    { id: 15, name: 'Magneta', skill:'Manejar los metales' },
+    { id: 16, name: 'RubberMan', skill:'Hablar con animales' },
+    { id: 17, name: 'Dynama', skill:'Piel de diamante' },
+    { id: 18, name: 'Dr IQ', skill:'SuperInteligencia' },
+    { id: 19, name: 'Magma', skill:'Manipular el fuego' },
+    { id: 20, name: 'Tornado', skill:'Super Velocidad' },
+];
+
